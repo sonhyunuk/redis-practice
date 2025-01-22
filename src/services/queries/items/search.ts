37457f083e81de3e9d02e1c4acf70446e2e51502
손현욱 <hus0316@ariatech.kr>
2025-01-22 10:15:09 +0900

    검색 로직 추가

--- src/services/queries/items/search.ts
+++ src/services/queries/items/search.ts
@@ -1,1 +1,33 @@
-export const searchItems = async (term: string, size: number = 5) => {};
+import { client } from "$services/redis";
+import { deserialize } from "./deserialize";
+import { itemsIndexeKey } from "$services/keys";
+
+export const searchItems = async (term: string, size: number = 5) => {
+
+    const cleaned = term
+        .replaceAll(/[^a-zA-Z0-9]/g, '')
+        .trim()
+        .split(' ')
+        .map((word) => word ? `%${word}%` : '')
+        .join(' ')
+
+
+    if(cleaned === '') {
+        return []
+    }
+
+    const results = await client.ft.search(
+        itemsIndexeKey(),
+        cleaned,
+        {
+            LIMIT : {
+                from : 0,
+                size,
+            }
+        }
+        
+    )
+
+    return results.documents.map(({id, value})=> deserialize(id, value as any))
+
+};
